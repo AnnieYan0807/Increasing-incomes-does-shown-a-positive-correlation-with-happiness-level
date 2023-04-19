c113b7341d609ac798cf396f44c2d5df283bdfcc
Annie Yan <yanzixin0807@gmail.com>
2023-04-18 21:16:45 -0400

    Update changes

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,61 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Data
+# Author: Annie Yan
+# Data: 16 April 2023 
+# Contact: zixin.yan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:None
+# Any other information needed?None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(853)
+
+simulated_data <-
+  tibble(
+    id =
+      rpois(
+        n = 365 * 3,
+        lambda = 10
+      ),
+    
+    income16 = c(
+      rep(x = "AVERAGE", times = 365),
+      rep(x = "BELOW AVERAGE", times = 365),
+      rep(x = "ABOVE AVERAGE", times = 365)
+    ),
+    
+    income = c(
+      rep(x = "$15,000 to $19,999", times = 365),
+      rep(x = "$20,000 to $24,999", times = 365),
+      rep(x = "$25,000 or more", times = 365)
+    ),
+    
+    rincome = c(
+      rep(x = "$4000 to 4999", times = 365),
+      rep(x = "$5000 to 5999", times = 365),
+      rep(x = "$6000 to 6999", times = 365)
+    ),
+    
+    happy = c(
+      rep(x = "Not too happy", times = 365),
+      rep(x = "Pretty happy", times = 365),
+      rep(x = "Very happy", times = 365)
+    ),
+    
+    MentalHealth = c(
+      rep(x = "Good", times = 365),
+      rep(x = "Stable", times = 365),
+      rep(x = "Poor", times = 365)
+    )
+    
+  )
+
+
+head(simulated_data)
 
 
 
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,24 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from GSS
+# Author: Annie Yan
+# Data: 16 April 2023 
+# Contact: zixin.yan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites:None
+# Any other information needed?None
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+library(readxl)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(haven)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+GSS_raw_data <- read_excel(here::here("Inputs/data/GSS.xlsx"))
 
 
 #### Save data ####
 # [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(GSS_raw_data, "Inputs/data/GSS_raw_data.csv") 
 
          
